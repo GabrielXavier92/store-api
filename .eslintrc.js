60971572143f45952bb42cbdcca414a6197ca373
Gabriel Xavier <gabriel.xavier@pipefy.com>
2021-03-12 18:06:11 -0300

    add rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,6 @@ module.exports = {
     'import/extensions': 0,
     'max-len': [2, 130, 4, { ignoreUrls: true }],
     'no-underscore-dangle': 'off',
+    'implicit-arrow-linebreak': 'off',
   },
 };
