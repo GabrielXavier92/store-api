f299e3c4134434bafff9b232e93babbe745be862
Gabriel Xavier <gabriel.xavier@pipefy.com>
2021-03-14 14:27:22 -0300

    create methods to create and login user

--- src/modules/auth/providers/Auth.ts
+++ src/modules/auth/providers/Auth.ts
@@ -1,22 +1,36 @@
+import { UserInputError } from 'apollo-server-errors';
 import { Injectable } from 'graphql-modules';
 import { AuthModule } from '../types';
 
 @Injectable()
 export class AuthProvider {
-  user = [];
+  user: [AuthModule.SignUpInput] = [{ email: 'a', password: 'b' }];
 
   createUser(data: AuthModule.SignUpInput): AuthModule.Auth {
-    const { fullName, email, password } = data;
-    console.log(fullName, email, password);
+    const { email } = data;
+
+    const user = this.user.find((user) => user.email === email);
+    if (user) throw new UserInputError('Failed to create account');
+
+    this.user.push(data);
+
     return {
-      token: 'asdadasad',
+      token: 'createUserToken',
     };
   }
 
   logIn(data: AuthModule.SignInInput): AuthModule.Auth {
-    console.log(data);
+    const { email, password } = data;
+    const user = this.user.find((user) => user.email === email);
+
+    if (!user || user.password !== password) throw new UserInputError('Failed to login');
+    /* TODO
+      injetar provider CLINIC e inserir as clinics/roles que o User pertence
+      colocar isso no token e retornar para o usuario 
+    */
+
     return {
-      token: 'asdasdad',
+      token: 'logInToken',
     };
   }
 }
