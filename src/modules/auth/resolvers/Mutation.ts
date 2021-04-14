48f2df01e5ca17cfd4d4852fed4c24362e157c2c
Gabriel Xavier <gabriel.xavier@pipefy.com>
2021-04-14 17:54:30 -0300

    remove mongo mutation

--- src/modules/auth/resolvers/Mutation.ts
+++ src/modules/auth/resolvers/Mutation.ts
@@ -1,8 +1,6 @@
 import { AuthModule } from '../types';
 import { AuthProvider } from '../providers';
 
-import UserSchema from '../schemas/user';
-
 const MutationResolver: AuthModule.Resolvers = {
   Mutation: {
     signIn(_, { signInInput }, { injector }: GraphQLModules.Context) {
@@ -10,11 +8,6 @@ const MutationResolver: AuthModule.Resolvers = {
     },
     signUp(_, { signUpInput }, { injector }: GraphQLModules.Context) {
       return injector.get(AuthProvider).createUser(signUpInput);
-    },
-    async mongoTeste(_, { signUpInput }) {
-      const { password, email, fullName } = signUpInput;
-      const user = await UserSchema.create({ password, email, fullName });
-      return !!user;
     },
   },
 };
